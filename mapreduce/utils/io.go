9fb5ffda76f0f286b1978ae96ae1b8ad0dcfe17c
Ato Aidoo <atoaidook@gmail.com>
2025-02-08 17:30:23 -0500

    Experimentation

--- mapreduce/utils/io.go
+++ mapreduce/utils/io.go
@@ -1,8 +1,14 @@
 package utils
 
+import application "github.com/atoaidoocr7/distributed-systems/mapreduce/app"
+
 // Everything io related
 
 type IOUtils struct {
 }
 
-func NewIOUtils() {}
+func NewIOUtils() {
+	m := application.Master{}
+
+	m.Start()
+}
