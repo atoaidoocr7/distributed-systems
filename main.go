48f5c4122f8c01f7634429839c832183a95b7885
Ato Aidoo <atoaidook@gmail.com>
2025-02-08 17:41:14 -0500

    Experiment

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import "fmt"
+import application "github.com/atoaidoocr7/distributed-systems/mapreduce/app"
 
 func main() {
 	fmt.Println("Hello World")
+	m := application.Master{}
+	m.Start()
 }
