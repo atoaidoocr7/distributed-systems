010d4e9fc2b5e975cb56e1497c14206b97e36d4d
Ato Aidoo <ato.aidoo@datadoghq.com>
2025-10-12 20:25:24 -0700

    Initial log stub

---
+++ raft/api/log.go
@@ -0,0 +1,45 @@
+package api
+
+import "errors"
+
+// TODO(atoaidoocr7): Look into how we can make these fields private. For now, the fields must be public
+// inorder to be able to use them with golangs net/rpc pacakge. The net/rpc package requires fields to be
+// exported in order to encode and decode request parameters
+type LogEntry struct {
+	term  int64
+	index int64
+	data  []byte
+}
+
+type Log struct {
+	entries []*LogEntry
+}
+
+func (l *Log) AddEntry(entry *LogEntry) {
+	l.entries = append(l.entries, entry)
+}
+
+func (l *Log) GetEntry(index int64) (LogEntry, error) {
+	if index < 0 || index >= int64(len(l.entries)) {
+		return LogEntry{}, errors.New("index out of range")
+	}
+	return *l.entries[index], nil
+}
+
+func (l *Log) Len() int {
+	return len(l.entries)
+}
+
+func (l *Log) Pop() bool {
+	if len(l.entries) == 0 {
+		return false
+	}
+	return true
+}
+
+func (l *Log) Peek() LogEntry {
+	if len(l.entries) == 0 {
+		return LogEntry{}
+	}
+	return *l.entries[0]
+}
