0388fbb4e6b2037039f87a82f074a33c51be6fa5
Ato Aidoo <ato.aidoo@datadoghq.com>
2025-10-12 21:11:54 -0700

    NewRaftServer function

--- raft/server/server.go
+++ raft/server/server.go
@@ -5,7 +5,21 @@ import (
 	"github.com/atoaidoocr7/distributed-systems/raft/server/util"
 )
 
+// RaftServer represents a very basic raft server
 type RaftServer struct {
 	serverType util.ServerType
-	log        api.Log
+	log        *api.Log
+	serverTerm int
+	serverId   string
+	nextIndex  map[string]int
+	matchIndex map[string]int
+}
+
+func NewRaftServer(serverName string) *RaftServer {
+	return &RaftServer{
+		serverType: util.Follower,
+		log:        api.NewLog(),
+		serverTerm: 0,
+		serverId:   serverName,
+	}
 }
